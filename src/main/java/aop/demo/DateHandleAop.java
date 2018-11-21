05bad752868eb247dfae61076c3c8cb962a4f4c1
sail456852 <sail456852@hotmail.com>
2018-11-21 17:03:14 +0800

    spring framework introducted and AOP

---
+++ src/main/java/aop/demo/DateHandleAop.java
@@ -0,0 +1,11 @@
+package aop.demo;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ * User: Administrator<br/>
+ * Date: 2018/11/21<br/>
+ * Time: 14:38<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class DateHandleAop {
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form  action="file/upload.do" method="post" enctype="multipart/form-data">
+
+<input  type="file" name="file"/>
+
+<input type="submit" value="commit"/>
+
+</form>
+
+</body>
+</html>
