f7603ca0348e8ee0a62746971ce5cc56198aad52
sail456852 <sail456852@hotmail.com>
2018-11-25 22:38:01 +0800

    helo

---
+++ src/main/java/tij/enumeration/exercise/five/VowelsAndConstants.java
@@ -0,0 +1,11 @@
+package tij.enumeration.exercise.five;
+
+/**
+ * Created by Okita.<br/>
+ * User: yz<br/>
+ * Date: 11/24/18<br/>
+ * Time: 1:01 PM<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class VowelsAndConstants {
+}
