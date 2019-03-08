923a253a36b924b85f2fc99c2bdff4f90db41174
sail456852 <sail456852@hotmail.com>
2019-03-08 17:06:53 +0800

    cookie to redis

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * run by java command line, with package name set it won't find the main function,
+ * as for why I don't understand it yet.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.err.println("JavaCompileDemo.main");
+    }
+}
